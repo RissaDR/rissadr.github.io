977da5724829e2e9011a299b9c76410a67e81ae3
RissaDR <57639793+RissaDR@users.noreply.github.com>
2020-06-09 17:55:23 +0200

    Add files via upload

--- projects/flutter/slidepuzzle/flutter_service_worker.js
+++ projects/flutter/slidepuzzle/flutter_service_worker.js
@@ -1,13 +1,13 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "4a86e97524038a8ae7b18d9f18fbacf9",
+  "main.dart.js": "3545cb499a94f1d40ecb47f3f5f62ad9",
 "manifest.json": "5da2e44265d800e32e11d5f5204201bc",
 "index.html": "bee0b674c5943069502e375382614610",
 "/": "bee0b674c5943069502e375382614610",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "34da31f697be5f2fcfacf877df9adb0a",
+"assets/LICENSE": "753f421b1d8ffb4ca2597a589aa2819f",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
